ade9bb86b269a63e0b7c5d885b05706973721988
Benjamin Schulte <benjamin@dokomi.de>
2017-11-14 14:02:28 +0100

    [FEATURE] introduce new symbol file format

--- src/SymbolListWriter.js
+++ src/SymbolListWriter.js
@@ -9,7 +9,9 @@ export default class SymbolListWriter {
 
     var pad = "0000"
 
-    result.push('[labels]');
+    result.push('#SNES65816')
+    result.push('')
+    result.push('[SYMBOL]');
     const symbols = this.symbols.all();
     for (let name in symbols) {
       const fullAddress = symbols[name];
@@ -17,17 +19,17 @@ export default class SymbolListWriter {
       const bank = (fullAddress >> 16).toString(16);
       const address = (fullAddress & 0xFFFF).toString(16);
 
-      result.push(`${pad.substr(0,2-bank.length)}${bank}:${pad.substr(0,4-address.length)}${address} ${name}`);
+      result.push(`${pad.substr(0,2-bank.length)}${bank}:${pad.substr(0,4-address.length)}${address} ${name} ANY 1`);
     }
 
     result.push('')
     if (this.commands.all().length) {
-      result.push('[debug]');
+      result.push('[COMMAND]');
 
       var index = 0;
       for (let command of this.commands.all()) {
         var indexStr = index.toString(16);
-        result.push(`${pad.substr(0,4-indexStr.length)}${indexStr} ${command}`);
+        result.push(`${pad.substr(0,4-indexStr.length)}${indexStr} ${JSON.stringify(command)}`);
         index++;
       }
     }
